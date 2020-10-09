9af5b489bf42a4c9fc578f5e3d30925e67b3019a
Dikz <cicerogabriel12@hotmail.com>
2020-10-08 23:43:23 -0300

    feat: add watson natural language undestanting service

--- robots/text.js
+++ robots/text.js
@@ -1,10 +1,22 @@
 const algorithmia = require('algorithmia')
 const setenceBoundaryDetection = require('sbd')
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1')
+const { IamAuthenticator } = require('ibm-watson/auth')
+
+const watsonApiKey = process.env.WAPIKEY
+
+const nlu = new NaturalLanguageUnderstandingV1({
+  authenticator: new IamAuthenticator({ apikey: watsonApiKey }),
+  version: '2019-02-01',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
 
 async function robot(content) {
   await fetchContentFromWikipedia(content)
   sanitizeContent(content)
   breakContentIntoSentences(content)
+  limitMaximumSentences(content)
+  await fetchKeywordsOfAllSentences(content)
 
   async function fetchContentFromWikipedia(content) {
     const algorithmiaAuthenticaded = algorithmia(process.env.APIKEY)
@@ -49,6 +61,38 @@ async function robot(content) {
         text: sentence,
         keywords: [],
         images: []
+      })
+    })
+  }
+
+  function limitMaximumSentences(content) {
+    content.sentences = content.sentences.slice(0, content.maximumSentences)
+  }
+
+  async function fetchKeywordsOfAllSentences(content) {
+    for (const sentence of content.sentences) {
+      sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+    }
+  }
+
+  async function fetchWatsonAndReturnKeywords(sentence) {
+    return new Promise((resolve, reject) => {
+      nlu.analyze({
+        text: sentence,
+        features: {
+          keywords: {}
+        }
+      }, (error, response) => {
+        if (error) {
+          reject(error)
+          return
+        }
+
+        const keywords = response.result.keywords.map((keyword) => {
+          return keyword.text
+        })
+
+        resolve(keywords)
       })
     })
   }
